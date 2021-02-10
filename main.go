7e78739616d56b2c6c7f917c789d98f22ad4ee93
Diego Lemos <diego@diegolemos.net>
2021-02-10 17:01:13 +0000

    Implement decimal > roman converter

--- main.go
+++ main.go
@@ -2,11 +2,50 @@ package main
 
 import "fmt"
 
+var romanDic = map[int]string{
+  1000: "M",
+  900 : "CM",
+  500 : "D",
+  400 : "CD",
+  100 : "C",
+  90  : "XC",
+  50  : "L",
+  40  : "XL",
+  10  : "X",
+  9   : "IX",
+  5   : "V",
+  4   : "IV",
+  1   : "I",
+}
+
+var decimalsDec = [...]int{
+  1000,
+  900 ,
+  500 ,
+  400 ,
+  100 ,
+  90  ,
+  50  ,
+  40  ,
+  10  ,
+  9   ,
+  5   ,
+  4   ,
+  1   ,
+}
+
 func main() {
 
   fmt.Println("hello")
 }
 
-func convert(d string) (string, error) {
-  return "", fmt.Errorf("not implemented")
+func convert(d int) (string) {
+
+  for _, k := range decimalsDec {
+    if d - k >= 0 {
+      return romanDic[k] + convert(d - k)
+    }
+  }
+
+  return ""
 }
--- main_test.go
+++ main_test.go
@@ -2,11 +2,43 @@ package main
 
 import "testing"
 
+var samples = map[int]string{
+  1: "I",
+  2: "II",
+  4: "IV",
+  5: "V",
+  6: "VI",
+  7: "VII",
+  8: "VIII",
+  9: "IX",
+  10: "X",
+  11: "XI",
+  14: "XIV",
+  19: "XIX",
+  20: "XX",
+  30: "XXX",
+  50: "L",
+  90: "XC",
+  100: "C",
+  400: "CD",
+  500: "D",
+  900: "CM",
+  1000: "M",
+  2021: "MMXXI",
+}
+
 func TestConvert(t *testing.T) {
+  for input, expected := range samples {
+    actual := convert(input)
 
-  _, err := convert("1")
-
-  if err != nil {
-    t.Errorf("unexpected error: %s", err)
+    if actual != expected {
+      t.Errorf("actual: %s\texpected: %s", actual, expected)
+    }
   }
+
+
+//   if err != nil {
+//     t.Errorf("unexpected error: %s", err)
+//   }
+
 }
